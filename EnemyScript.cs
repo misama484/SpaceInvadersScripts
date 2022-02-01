a95ee40d38f6bbf0c752496174c71e0556a12c72
Zephyrus\Miguel <misan484@hotmail.com>
2022-02-01 09:56:26 +0100

    Ejercicio_8

--- EnemyScript.cs
+++ EnemyScript.cs
@@ -4,10 +4,7 @@ using UnityEngine;
 
 public class EnemyScript : MonoBehaviour
 {
-    //booleana que indicara cuando ha llegado al limite de la pantalla, para volver.
-    bool mueveDerecha = true;
-    //inidica el tiempo que pasa para iniciar el movimiento lateral de nuevo(corrutina)
-    float sleep = 0.5f;
+
 
     GameObject player;
     GameObject youWin;
@@ -17,11 +14,6 @@ public class EnemyScript : MonoBehaviour
   
     void Start()
     {
-        //ejecuta la funcion moveEnemy a los 0.5 segundos, y lo repite cada 0.5 segs.
-        //InvokeRepeating("moveEnemy", 0.5f, 0.5f);
-
-        //Con corrutinas:        
-            StartCoroutine(Wait());
 
         youWin = GameObject.FindGameObjectWithTag("YouWin");
         youWin.SetActive(false);
@@ -37,43 +29,7 @@ public class EnemyScript : MonoBehaviour
         
     }
 
-    public IEnumerator Wait()
-    {
-        yield return new WaitForSeconds(sleep);
-        MoveEnemy();
-        StartCoroutine(Wait());
-    }
-
-   void MoveEnemy()
-    {
-        if (mueveDerecha)
-        {            
-            if(transform.position.x > 6.5)
-            {
-                sleep -= 0.05f;    //restamos tiempo a la corrutina cuando llegue al limite de pantalla, para aumentar velocidad
-                mueveDerecha = false;
-                transform.Translate(Vector3.down);  //bajamos una linea los enemigos
-            }
-            else
-            {
-                transform.Translate(Vector3.right); //movemos a la derecha
-            }
-        }
-        else
-        {
-            
-            if(transform.position.x < -6.5)
-            {
-                sleep -= 0.05f;
-                mueveDerecha =true;
-                transform.Translate(Vector3.down);
-            }
-            else
-            {
-                transform.Translate(Vector3.left);
-            }
-        }
-    }
+   
     //metodo que controla que cuando SOLO un misil toque el enemigo, se destruya.
     private void OnTriggerEnter2D(Collider2D collision)
     {
@@ -83,7 +39,7 @@ public class EnemyScript : MonoBehaviour
             Destroy(gameObject);
             //activamos el cartel
             youWin.SetActive(true);
-            //al poner el contador a 1, evitamos que pueda volver a disparar
+            //al poner el contador a 1, evitamos que pueda volver a disparar 
             spaceShipScript.contador = 1;
         }
         
---
+++ HordaScript.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HordaScript : MonoBehaviour
+{
+    //booleana que indicara cuando ha llegado al limite de la pantalla, para volver.
+    bool mueveDerecha = true;
+    //inidica el tiempo que pasa para iniciar el movimiento lateral de nuevo(corrutina)
+    float sleep = 0.5f;
+    void Start()
+    {
+        //ejecuta la funcion moveEnemy a los 0.5 segundos, y lo repite cada 0.5 segs.
+        //InvokeRepeating("moveEnemy", 0.5f, 0.5f);
+
+        //Con corrutinas:        
+        StartCoroutine(Wait());
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public IEnumerator Wait()
+    {
+        yield return new WaitForSeconds(sleep);
+        MoveEnemy();
+        StartCoroutine(Wait());
+    }
+
+    void MoveEnemy()
+    {
+        if (mueveDerecha)
+        {
+            if (transform.position.x > 4.5)
+            {
+                sleep -= 0.05f;    //restamos tiempo a la corrutina cuando llegue al limite de pantalla, para aumentar velocidad
+                mueveDerecha = false;
+                transform.Translate(Vector3.down);  //bajamos una linea los enemigos
+            }
+            else
+            {
+                transform.Translate(Vector3.right); //movemos a la derecha
+            }
+        }
+        else
+        {
+
+            if (transform.position.x < -4.5)
+            {
+                sleep -= 0.05f;
+                mueveDerecha = true;
+                transform.Translate(Vector3.down);
+            }
+            else
+            {
+                transform.Translate(Vector3.left);
+            }
+        }
+    }
+}
